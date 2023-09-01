07bafa25b65a09c845ac567d0468571bb5e31d57
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-09-01 22:38:40 +0700

    refactor: define route as global constant

--- src/components/Sidebar/index.constants.tsx
+++ src/components/Sidebar/index.constants.tsx
@@ -1,14 +1,15 @@
+import { CARTS_ROUTE, PRODUCTS_ROUTE } from "@/constants/routes";
 import { FiLayers, FiShoppingCart } from "react-icons/fi";
 
 export const SIDEBAR_MENUS = [
   {
     label: "Products",
-    route: "/products",
+    route: PRODUCTS_ROUTE,
     icon: <FiLayers />,
   },
   {
     label: "Carts",
-    route: "/carts",
+    route: CARTS_ROUTE,
     icon: <FiShoppingCart />,
   },
 ];
---
+++ src/constants/routes.ts
@@ -0,0 +1,2 @@
+export const PRODUCTS_ROUTE = "/products";
+export const CARTS_ROUTE = "/cart";
